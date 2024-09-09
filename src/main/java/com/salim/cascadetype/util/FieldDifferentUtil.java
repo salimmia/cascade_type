fb2585295b4ca311dbe24b0f455fd7f59f346e79
Muhammad Salim <salimmia3745@gmail.com>
2024-09-09 11:36:06 +0600

    updateIfDifferent added

--- src/main/java/com/salim/cascadetype/util/FieldDifferentUtil.java
+++ src/main/java/com/salim/cascadetype/util/FieldDifferentUtil.java
@@ -3,6 +3,7 @@ package com.salim.cascadetype.util;
 import org.springframework.util.StringUtils;
 
 import java.util.Objects;
+import java.util.function.Consumer;
 
 public class FieldDifferentUtil {
     public static <T> boolean isDifferent(T newValue, T oldValue) {
@@ -10,5 +11,11 @@ public class FieldDifferentUtil {
             return StringUtils.hasText((String) newValue) && !Objects.equals(newValue, oldValue);
         }
         return newValue != null && !Objects.equals(oldValue, newValue);
+    }
+
+    public static <T> void updateIfDifferent(T newValue, T oldValue, Consumer<T> updater) {
+        if (isDifferent(newValue, oldValue)) {
+            updater.accept(newValue);
+        }
     }
 }
